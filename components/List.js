610ad48339356fdff4267708a3a1a7134663f495
Dreamweaver-jun <564171391@qq.com>
2018-06-09 23:35:16 +0800

    demo

---
+++ components/List.js
@@ -0,0 +1,13 @@
+define(['vue','text!../components/list.html', '../components/nav'],function(Vue, listTpl, nav){
+    Vue.component('list',{
+        template:listTpl,
+        data: function(){
+            return {
+                list: [2,4,6,8]
+            }
+        },
+        components: {
+            'my-nav': nav
+        }
+    })
+});
